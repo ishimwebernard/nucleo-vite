5fcee3e205b6f3ac60ec43f0de3888f651be0757
ishimwebernard <71332125+ishimwebernard@users.noreply.github.com>
2023-02-21 09:51:46 +0200

    1 file modified, 7 added

---
+++ src/qn2.jsx
@@ -0,0 +1,101 @@
+import { useState } from 'react'
+import { RadioGroup } from '@headlessui/react'
+
+const plans = [
+  {
+    name: 'Urban  ',
+    desc: 'For urban areas, solar energy can be a good option as it can be installed on rooftops and other unused spaces. Nuclear energy may not be practical for urban areas due to concerns about safety and waste management.  '
+
+  },
+  {
+    name: 'Suburban  ',
+    desc: 'Wind energy can be a good option for suburban areas, as there is often more open space available for wind turbines. Nuclear energy can also be a viable option for suburban areas.  '
+  },
+  {
+    name: 'Rural  ',
+    desc: 'Biomass energy can be a good option for rural areas, as there is often an abundance of agricultural and forestry waste that can be used for fuel. Nuclear energy can also be a viable option for rural areas.'
+  },
+]
+
+export default function Example() {
+  const [selected, setSelected] = useState(plans[0])
+
+  return (
+    <div className="w-full px-4 py-16">
+      <div className="mx-auto w-full max-w-md">
+        <RadioGroup value={selected} onChange={setSelected}>
+          <RadioGroup.Label className="sr-only">Server size</RadioGroup.Label>
+          <div className="space-y-2">
+            {plans.map((plan) => (
+              <RadioGroup.Option
+                key={plan.name}
+                value={plan}
+                className={({ active, checked }) =>
+                  `${
+                    active
+                      ? 'ring-2 ring-white ring-opacity-60 ring-offset-2 ring-offset-sky-300'
+                      : ''
+                  }
+                  ${
+                    checked ? 'bg-sky-900 bg-opacity-75 text-white' : 'bg-white'
+                  }
+                    relative flex cursor-pointer rounded-lg px-5 py-4 shadow-md focus:outline-none`
+                }
+              >
+                {({ active, checked }) => (
+                  <>
+                    <div className="flex w-full items-center justify-between">
+                      <div className="flex items-center">
+                        <div className="text-sm">
+                          <RadioGroup.Label
+                            as="p"
+                            className={`font-medium  ${
+                              checked ? 'text-white' : 'text-gray-900'
+                            }`}
+                          >
+                            {plan.name}
+                          </RadioGroup.Label>
+                          <RadioGroup.Description
+                            as="span"
+                            className={`inline ${
+                              checked ? 'text-sky-100' : 'text-gray-500'
+                            }`}
+                          >
+                            <span>
+                              {plan.desc}/{plan.cpus}
+                            </span>{' '}
+                            
+                          </RadioGroup.Description>
+                        </div>
+                      </div>
+                      {checked && (
+                        <div className="shrink-0 text-white">
+                          <CheckIcon className="h-6 w-6" />
+                        </div>
+                      )}
+                    </div>
+                  </>
+                )}
+              </RadioGroup.Option>
+            ))}
+          </div>
+        </RadioGroup>
+      </div>
+    </div>
+  )
+}
+
+function CheckIcon(props) {
+  return (
+    <svg viewBox="0 0 24 24" fill="none" {...props}>
+      <circle cx={12} cy={12} r={12} fill="#fff" opacity="0.2" />
+      <path
+        d="M7 13l3 3 7-7"
+        stroke="#fff"
+        strokeWidth={1.5}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  )
+}
---
+++ src/radiogroup.jsx
@@ -0,0 +1,101 @@
+import { useState } from 'react'
+import { RadioGroup } from '@headlessui/react'
+
+const plans = [
+  {
+    name: 'Low ',
+    desc: 'If the energy demand is low, then a renewable energy source such as solar energy might be a good option as it can be cost-effective and easy to install. Nuclear energy could also be an option, but may not be as cost-effective for low energy demand situations. '
+
+  },
+  {
+    name: 'Medium ',
+    desc: 'For medium energy demands, wind energy can be a better option, as it is more reliable and can generate a higher amount of energy than solar. Nuclear energy can also be an option for medium energy demand situations. '
+  },
+  {
+    name: 'High ',
+    desc: 'Hydroelectric energy can be a good option for high energy demand situations, as it can generate a large amount of energy with a consistent flow of water. Nuclear energy can also be a good option for high energy demand situations.'
+  },
+]
+
+export default function Example() {
+  const [selected, setSelected] = useState(plans[0])
+
+  return (
+    <div className="w-full px-4 py-16">
+      <div className="mx-auto w-full max-w-md">
+        <RadioGroup value={selected} onChange={setSelected}>
+          <RadioGroup.Label className="sr-only">Server size</RadioGroup.Label>
+          <div className="space-y-2">
+            {plans.map((plan) => (
+              <RadioGroup.Option
+                key={plan.name}
+                value={plan}
+                className={({ active, checked }) =>
+                  `${
+                    active
+                      ? 'ring-2 ring-white ring-opacity-60 ring-offset-2 ring-offset-sky-300'
+                      : ''
+                  }
+                  ${
+                    checked ? 'bg-sky-900 bg-opacity-75 text-white' : 'bg-white'
+                  }
+                    relative flex cursor-pointer rounded-lg px-5 py-4 shadow-md focus:outline-none`
+                }
+              >
+                {({ active, checked }) => (
+                  <>
+                    <div className="flex w-full items-center justify-between">
+                      <div className="flex items-center">
+                        <div className="text-sm">
+                          <RadioGroup.Label
+                            as="p"
+                            className={`font-medium  ${
+                              checked ? 'text-white' : 'text-gray-900'
+                            }`}
+                          >
+                            {plan.name}
+                          </RadioGroup.Label>
+                          <RadioGroup.Description
+                            as="span"
+                            className={`inline ${
+                              checked ? 'text-sky-100' : 'text-gray-500'
+                            }`}
+                          >
+                            <span>
+                              {plan.desc}/{plan.cpus}
+                            </span>{' '}
+                            
+                          </RadioGroup.Description>
+                        </div>
+                      </div>
+                      {checked && (
+                        <div className="shrink-0 text-white">
+                          <CheckIcon className="h-6 w-6" />
+                        </div>
+                      )}
+                    </div>
+                  </>
+                )}
+              </RadioGroup.Option>
+            ))}
+          </div>
+        </RadioGroup>
+      </div>
+    </div>
+  )
+}
+
+function CheckIcon(props) {
+  return (
+    <svg viewBox="0 0 24 24" fill="none" {...props}>
+      <circle cx={12} cy={12} r={12} fill="#fff" opacity="0.2" />
+      <path
+        d="M7 13l3 3 7-7"
+        stroke="#fff"
+        strokeWidth={1.5}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  )
+}
